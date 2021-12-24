24eeeebcc150c25f0ed5f5bf0cce28d83e38a0f9
Heechul Yun <heechul.yun@ku.edu>
2021-12-24 11:42:01 -0600

    smaller 'random_data' (still bigger than L2)

--- detect_spr_list.c
+++ detect_spr_list.c
@@ -148,11 +148,25 @@ struct div_test transmit;
  * default       9 (works on all tested platforms)
  */ 
 
-#include "random_data.h"
+// #include "random_data.h"
+unsigned int random_data[] = {
+     1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+    71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+    91, 92, 93, 94, 95, 0,
+};
+
 struct div_test *test_tasks[20];
 
 volatile char my_out[16];
-volatile int linked[2048][1024] __attribute__ ((aligned (4096)));
+// linked: 96 * 4K = 384K > L2 cache (256KB)
+volatile int linked[96][1024] __attribute__ ((aligned (4096)));
 
 int start = 0;
 
